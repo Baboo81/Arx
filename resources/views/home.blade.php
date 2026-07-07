89ee7338eba3f6b42cbfaafb644a42e4978db508
Baboo81 <magicbaboo@gmail.com>
2026-07-07 11:22:34 +0200

    landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,5 +12,15 @@
 @section('meta_description', 'Plate-forme de sécurisatiion du réseau domestique')
 
 @section('content')
-
+{{-- Section : Home --}}
+<section class="home">
+    <div class="container h-100">
+        <div class="row">
+            <div class="main-content">
+                
+            </div>
+        </div>
+    </div>
+</section>
+{{-- Section : Home END --}}
 @endsection
