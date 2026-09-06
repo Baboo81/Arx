6c32dad981858f5338412573372f3dda7b6c63d2
Baboo81 <magicbaboo@gmail.com>
2026-09-06 15:30:55 +0200

    arx/ai

---
+++ resources/views/arx/ai.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@push('styles')
+    @vite(['ressources/css/reset.css', 'ressources/css/ai.css'])
+@endpush
+
+@section('title', 'ARX | ARX AI')
+
+@section('meta_description', 'ARX AI')
+
+@section('content')
+
+@endsection
