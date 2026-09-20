cad92e6d1431101623b1b771ef28b312b9bca655
Baboo81 <magicbaboo@gmail.com>
2026-09-20 14:26:02 +0200

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,5 +39,6 @@
                 @endisset
             </main>
         </div>
+        @include('partials.footer')
     </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="row text-center">
+        <div class="col-12 col-sm-6 col-md-4">
+            <section class="flex-column">
+                
+            </section>
+        </div>
+    </div>
+</footer>
